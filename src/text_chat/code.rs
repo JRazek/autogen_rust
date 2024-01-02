ca986e510bc26c9297ff3127e964eceec91d475f
JRazek <jakub.razek2@gmail.com>
2024-01-02 14:01:25 +0100

    code executor comment

--- src/text_chat/code.rs
+++ src/text_chat/code.rs
@@ -1,3 +1,4 @@
+/// Language is kept as a string for now.
 #[derive(Debug, Clone)]
 pub struct CodeBlock {
     pub language: String,
@@ -9,6 +10,13 @@ pub enum CodeBlockExecutionResult {
     Success(String),
     Failure(String),
 }
+
+/// One may define a custom executor for a specific platform.
+/// For example, one may define executor that runs on local machine,
+/// remote machine, or a docker container.
+///
+/// This is very similar to the UserProxyAgent, however since its a trait, it does not care about
+/// any specific implementation and configs as the Python version does.
 
 pub trait CodeExecutor {
     type Error;
